035b892c0acb5b8c01c41c8d8c42ae90a303b4de
Kelly Moore <kellygarskemoore@Kellys-MacBook-Pro.local>
2015-11-13 08:31:56 -0600

    templates and routes working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,19 @@
 
-var myApp = angular.module('myApp', ['as.sortable']);
+
+var myApp = angular.module("myApp", ['as.sortable', 'ngRoute']);
+
+myApp.config(["$routeProvider", function($routeProvider){
+    $routeProvider.
+        when('/home', {
+            templateUrl: "assets/views/routes/home.html"
+        }).
+        when('/sortbynumber', {
+            templateUrl: "assets/views/routes/sortbynumber.html"
+        }).
+        otherwise({
+            redirectTo: 'home'
+        })
+}]);
 
 myApp.controller('MainCtrl', ["$scope", function ($scope) {
 
@@ -51,6 +65,16 @@ myApp.controller('MainCtrl', ["$scope", function ($scope) {
         name: 'C'
     }, {
         name: 'I'
+    }];
+
+    $scope.numberlist1 = [{
+        name: '67+24+18'
+    } , {
+        name: '1000/10'
+    } , {
+        name: '4x4x4'
+    } , {
+        name: '450-220-43'
     }];
 
 
